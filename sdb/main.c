2cd8d7583f5fef95687e0660d5dcbcd81f116e59
Prabhu <123748142+sibhu-88@users.noreply.github.com>
2024-10-15 09:52:19 +0530

    Update main.c

--- sdb/main.c
+++ sdb/main.c
@@ -1,5 +1,47 @@
 #include "header.h"
 
+
+void menu1(){
+
+	printf("\n\t****STUDENT RECORD MENU****\n\n");
+	printf("\t  a/A : Add New Record\n");
+	printf("\t  d/D : Delete A Record\n");
+	printf("\t  s/S : Show The List\n");
+	printf("\t  m/M : Modify A Record\n");
+	printf("\t  v/V : Save\n");
+	printf("\t  e/E : Exit\n");
+	printf("\t  t/T : Sort The List\n");
+	printf("\t  l/L : Delete All The Records\n");
+	printf("\t  r/R : Reverse The List\n\n");
+	printf("\t  Enter Your Choice : ");
+}
+
+void menu2(){
+	printf("\t  R/r : Enter Roll To Delete\n");
+        printf("\t  N/n : Enter Name To Delete\n");
+	printf("\t  Enter : ");
+}
+
+void menu3(){
+	printf("\t  Enter Which Record To Search Fo Nodification\n");
+	printf("\t  R/r : To Search A RollNo\n");
+        printf("\t  N/n : To Search A Name\n");
+	printf("\t  P/P : To Search A percentage\n");
+        printf("\t  Enter : ");
+}
+
+void menu4(){
+	printf("\t  S/s : Save And Exit\n");
+        printf("\t  E/e : Exit Without Saving\n");
+        printf("\t  Enter : ");
+}
+
+void menu5(){
+        printf("\t  N/n : Sort With Name\n");
+	printf("\t  P/p : Sort With Percentage\n");
+	printf("\t  Enter : ");
+}
+
 int main(){
 	char op;
 	SDB *hptr=0;
