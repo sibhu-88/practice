b5ec5e12ca6b40dcd6f0d5f95ad4618455c2c3fb
Prabhu <123748142+sibhu-88@users.noreply.github.com>
2024-10-15 11:07:48 +0530

    Update add.c

--- sdb/add.c
+++ sdb/add.c
@@ -1,6 +1,26 @@
 #include "header.h"
 
 void Add_new_record(SDB **ptr){
-	printf("Add record\n");
+
+	SDB *temp= (SDB *)malloc(sizeof(SDB));
+
+	printf("\tEnter Your Name : ");
+	scanf(" %[^\n]",temp->name);
+	printf("\tEnter Your Persentage : ");
+	scanf("%d",&temp->persentage);
+
+	if(*ptr==0){
+		*ptr = temp;
+		temp->next=0;
+		temp->rollno =1;
+	}else{
+
+		SDB *last=*ptr;
+		while(last->!=0) last = last->next;
+
+		temp->rollno = (last->rollno + 1);
+		temp->next = last->next;
+		last->next = temp;
+	}
 
 }
