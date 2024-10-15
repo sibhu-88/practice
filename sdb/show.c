5fd4e0865ef3a51a6942e66d0d2a2b813f72a3de
Prabhu <123748142+sibhu-88@users.noreply.github.com>
2024-10-15 11:35:44 +0530

    Update show.c

--- sdb/show.c
+++ sdb/show.c
@@ -1,7 +1,12 @@
 #include "header.h"
 
 void Show_list(SDB *ptr){
-	printf("Show list\n");
+	printf("\n\tShowing  list :\n");
+	printf("\tRoll.No\t\tName\t\tpercentage\n");
+	while(ptr!=0){
+		printf("\t%d\t\t%s\t\t%d\n",(ptr)->rollno,(ptr)->name,(ptr)->percentage);
+		ptr=ptr->next;
+	}
 }
 
 void Sort_list(SDB *ptr){
